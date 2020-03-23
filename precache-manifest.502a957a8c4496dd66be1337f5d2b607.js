0049fdde033e97f7d1c8fe8133acbe7bb6373289
Siridet <siridatsukkana@hotmail.com>
2020-03-23 16:35:55 +0700

    Updates

---
+++ precache-manifest.502a957a8c4496dd66be1337f5d2b607.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15e42a549ed5c02184fd982c148d7e4",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "b5f3cd134c34e0245f5e",
+    "url": "/covid/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d4d32a0910ac764172ec",
+    "url": "/covid/static/js/2.5d039645.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid/static/js/2.5d039645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f3cd134c34e0245f5e",
+    "url": "/covid/static/js/main.fca5cd01.chunk.js"
+  },
+  {
+    "revision": "cb89d5983b0aee4c0fd2",
+    "url": "/covid/static/js/runtime-main.f8e3727a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid/static/media/logo.5d5d9eef.svg"
+  }
+]);
